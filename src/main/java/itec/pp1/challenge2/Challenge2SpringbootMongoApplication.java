4293d449f61f34e92530a736a5434fcdcd8f9e6f
Mario Cabral <mariocabral.dev@gmail.com>
2021-09-18 19:49:35 -0300

    init project

---
+++ src/main/java/itec/pp1/challenge2/Challenge2SpringbootMongoApplication.java
@@ -0,0 +1,13 @@
+package itec.pp1.challenge2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge2SpringbootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge2SpringbootMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itec/pp1/challenge2/Challenge2SpringbootMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package itec.pp1.challenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge2SpringbootMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
